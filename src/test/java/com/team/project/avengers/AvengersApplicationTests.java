6e652e4536ede51f54e2089a8d8ceed4a6c3fc31
dearreally <rlaxodrnfl114@gmail.com>
2026-08-05 09:32:33 +0900

    cast-test

--- src/test/java/com/team/project/avengers/AvengersApplicationTests.java
+++ src/test/java/com/team/project/avengers/AvengersApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AvengersApplicationTests {
@@ -21,111 +21,115 @@ class AvengersApplicationTests {
 	}
 
 	@Test
-	void findAllCastTest() {
+	void insertCastDataTest() {
 
-		List<Cast> castList =
-				castRepository.findAllByOrderByDisplayOrderAsc();
+		// 데이터가 존재하면 중복 입력하지 않고 테스트 종료
+		if (castRepository.count() > 0) {
+			System.out.println("이미 출연진 및 제작진 데이터가 존재합니다.");
+			return;
+		}
 
-		castList.forEach(this::printCast);
-
-		assertEquals(10, castList.size());
-	}
-
-	@Test
-	void findActorListTest() {
-
-		List<Cast> actorList =
-				castRepository
-						.findByCastTypeOrderByDisplayOrderAsc("ACTOR");
-
-		actorList.forEach(this::printCast);
-
-		assertEquals(6, actorList.size());
-
-		assertTrue(
-				actorList.stream()
-						.allMatch(cast ->
-								"ACTOR".equals(cast.getCastType()))
+		List<Cast> castList = List.of(
+				createCast(
+						"로버트 다우니 주니어",
+						"토니 스타크 / 아이언맨",
+						"ACTOR",
+						"/image/cast/robert-downey-jr.jpg",
+						1
+				),
+				createCast(
+						"크리스 에반스",
+						"스티브 로저스 / 캡틴 아메리카",
+						"ACTOR",
+						"/image/cast/chris-evans.jpg",
+						2
+				),
+				createCast(
+						"마크 러팔로",
+						"브루스 배너 / 헐크",
+						"ACTOR",
+						"/image/cast/mark-ruffalo.jpg",
+						3
+				),
+				createCast(
+						"크리스 헴스워스",
+						"토르",
+						"ACTOR",
+						"/image/cast/chris-hemsworth.jpg",
+						4
+				),
+				createCast(
+						"스칼렛 요한슨",
+						"나타샤 로마노프 / 블랙 위도우",
+						"ACTOR",
+						"/image/cast/scarlett-johansson.jpg",
+						5
+				),
+				createCast(
+						"제레미 레너",
+						"클린트 바튼 / 호크아이",
+						"ACTOR",
+						"/image/cast/jeremy-renner.jpg",
+						6
+				),
+				createCast(
+						"조스 웨던",
+						"감독 및 각본",
+						"CREW",
+						"/image/cast/joss-whedon.jpg",
+						7
+				),
+				createCast(
+						"케빈 파이기",
+						"제작",
+						"CREW",
+						"/image/cast/kevin-feige.jpg",
+						8
+				),
+				createCast(
+						"앨런 실베스트리",
+						"음악",
+						"CREW",
+						"/image/cast/alan-silvestri.jpg",
+						9
+				),
+				createCast(
+						"시머스 맥가비",
+						"촬영",
+						"CREW",
+						"/image/cast/default-profile.jpg",
+						10
+				)
 		);
+
+		castRepository.saveAllAndFlush(castList);
+
+		assertEquals(10, castRepository.count());
+
+		castRepository
+				.findAllByOrderByDisplayOrderAsc()
+				.forEach(this::printCast);
 	}
 
-	@Test
-	void findCrewListTest() {
+	private Cast createCast(
+			String castName,
+			String roleName,
+			String castType,
+			String imagePath,
+			Integer displayOrder
+	) {
+		Cast cast = new Cast();
 
-		List<Cast> crewList =
-				castRepository
-						.findByCastTypeOrderByDisplayOrderAsc("CREW");
+		cast.setCastName(castName);
+		cast.setRoleName(roleName);
+		cast.setCastType(castType);
+		cast.setImagePath(imagePath);
+		cast.setDisplayOrder(displayOrder);
 
-		crewList.forEach(this::printCast);
-
-		assertEquals(4, crewList.size());
-
-		assertTrue(
-				crewList.stream()
-						.allMatch(cast ->
-								"CREW".equals(cast.getCastType()))
-		);
-	}
-
-	@Test
-	void searchByCastNameTest() {
-
-		String keyword = "크리스";
-
-		List<Cast> searchResult =
-				castRepository
-						.findByCastNameContainingIgnoreCaseOrRoleNameContainingIgnoreCaseOrderByDisplayOrderAsc(
-								keyword,
-								keyword
-						);
-
-		searchResult.forEach(this::printCast);
-
-		assertEquals(2, searchResult.size());
-	}
-
-	@Test
-	void searchByRoleNameTest() {
-
-		String keyword = "아이언맨";
-
-		List<Cast> searchResult =
-				castRepository
-						.findByCastNameContainingIgnoreCaseOrRoleNameContainingIgnoreCaseOrderByDisplayOrderAsc(
-								keyword,
-								keyword
-						);
-
-		searchResult.forEach(this::printCast);
-
-		assertFalse(searchResult.isEmpty());
-
-		assertTrue(
-				searchResult.stream()
-						.anyMatch(cast ->
-								cast.getRoleName().contains(keyword))
-		);
-	}
-
-	@Test
-	void emptySearchResultTest() {
-
-		String keyword = "검색되지않는인물";
-
-		List<Cast> searchResult =
-				castRepository
-						.findByCastNameContainingIgnoreCaseOrRoleNameContainingIgnoreCaseOrderByDisplayOrderAsc(
-								keyword,
-								keyword
-						);
-
-		searchResult.forEach(this::printCast);
-
-		assertTrue(searchResult.isEmpty());
+		return cast;
 	}
 
 	private void printCast(Cast cast) {
-
 		System.out.println("--------------------------------");
 		System.out.println("번호: " + cast.getCastId());
 		System.out.println("이름: " + cast.getCastName());
